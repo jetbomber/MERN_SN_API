269a32117f5e07d5d8a0cf6c81b1b0bdc3864bd3
jetbomber <rwarwaruk#shaw.ca>
2020-09-21 18:34:12 -0600

    Changed routes to be /api

--- app.js
+++ app.js
@@ -1,56 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const expressValidator = require("express-validator");
-const fs = require('fs');
-const cors = require('cors');
-dotenv.config()
-
+const fs = require("fs");
+const cors = require("cors");
+dotenv.config();
 
 //db
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
-.then(()=> console.log('DB Connected'))
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected"));
 
-mongoose.connection.on('error',err=> {
-    console.log(`DB Connection error: ${err.message}`);
+mongoose.connection.on("error", (err) => {
+  console.log(`DB Connection error: ${err.message}`);
 });
 
 //bring in routes
-const postRoutes = require('./routes/post');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 //middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
-app.use('/', postRoutes);
-app.use('/', authRoutes);
-app.use('/', userRoutes);
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 //apiDocs
-app.get('/', (req,res) =>{
-  fs.readFile('docs/apiDocs.json',(err,data)=> {
+app.get("/api", (req, res) => {
+  fs.readFile("docs/apiDocs.json", (err, data) => {
     if (err) {
       res.status(400).json({
-        error: err
-      })
+        error: err,
+      });
     }
-    const docs = JSON.parse(data)
+    const docs = JSON.parse(data);
     res.json(docs);
-  })
+  });
 });
 
 app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).json({error: "Unauthorized!"});
-    }
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: "Unauthorized!" });
+  }
 });
 
-const port=process.env.PORT || 8080;
-app.listen(port, ()=>{console.log(`A Node Js API is listening on port: ${port}`)});
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`A Node Js API is listening on port: ${port}`);
+});
